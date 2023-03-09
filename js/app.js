dcc1b00447fa6d07b3357761aff29db20155a3c9
Williams Wallace <wwallace.dev@gmail.com>
2023-03-09 18:10:37 -0300

    It is done

--- js/app.js
+++ js/app.js
@@ -49,11 +49,9 @@ document.addEventListener('DOMContentLoaded',() => {
 .fromTo('.logo-hero',0.7,
 {
     opacity : 0,
-    y : -50,
 },
 {
     opacity : 1,
-    y : 0,
     ease : Expo.easeInOut,
 }, '-=.5')
 
@@ -110,3 +108,15 @@ document.addEventListener('DOMContentLoaded',() => {
 
 
 })
+
+var bg = document.querySelector('.icon-menu-bg');
+console.log(bg);
+var menu = document.querySelector('.navbar-mobile .nav-menu');
+console.log(menu);
+
+bg.addEventListener('click',()=>{
+    bg.classList.toggle('icon-menu-bg-active')
+    menu.classList.toggle('nav-menu-active')
+    console.log(menu)
+})
+
